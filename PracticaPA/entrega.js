1df809f1cc43ee641dcafe6cf955e61d814cec35
UADER_IS2_Cardinaux <127860186+NicolasCardinaux@users.noreply.github.com>
2024-08-20 23:51:43 -0300

    Update entrega.js

--- PracticaPA/entrega.js
+++ PracticaPA/entrega.js
@@ -1,4 +1,5 @@
-const apikey = "fdfd0ebc-f93a-4e5f-b5ef-f74f2412ade1";
+const apikey = "";
+
 
 async function fetchElements() {
     const response = await fetch(`https://rest.coinapi.io/v1/exchanges`, {
@@ -17,11 +18,12 @@ async function fetchElements() {
 }
 
 
+
 async function displayElements() {
     const cryptoList = document.getElementById('crypto-list');
     const cryptoData = await fetchElements();
 
-    if (cryptoData && cryptoData.length > 0){ //checks if truthy
+    if (cryptoData && cryptoData.length > 0){ 
         cryptoData.forEach(element => {
             const listItem = document.createElement("li");
             listItem.textContent = `Name: ${element.name}, Volume (1hr): $${element.volume_1hrs_usd}, Volume (1day): $${element.volume_1day_usd}`;
