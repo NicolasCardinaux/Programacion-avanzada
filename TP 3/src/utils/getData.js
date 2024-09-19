a1550f2629f38a2fbc318c66d0885f8220240b91
UADER_IS2_Cardinaux <127860186+NicolasCardinaux@users.noreply.github.com>
2024-09-19 20:16:14 -0300

    Update getData.js

--- TP 3/src/utils/getData.js
+++ TP 3/src/utils/getData.js
@@ -4,29 +4,29 @@ const getData = async (id = null) => {
     const apiURL = API;
 
     try {
-        const response = await fetch(apiURL);  // Realiza la solicitud a la API
-        const data = await response.json();    // Convierte la respuesta a JSON
+        const response = await fetch(apiURL);  
+        const data = await response.json();   
         
         if (id) {
-            // Si hay un `id` proporcionado, se filtra por él
+          
             const result = data.find(item => item.id === id);
 
             if (result) {
-                console.log(result);   // Mostrar el resultado si se encontró
-                return result;         // Devolver solo el objeto con el id buscado
+                console.log(result);   
+                return result;         
             } else {
                 console.log(`No se encontró un elemento con el id: ${id}`);
-                return null;           // Devolver null si no se encuentra el id
+                return null;        
             }
         } else {
-            // Si no se proporciona un `id`, devolver todos los datos
-            console.log(data);         // Mostrar todos los datos en la consola
-            return data;               // Devolver todos los datos
+ 
+            console.log(data);        
+            return data;               
         }
 
     } catch(error) {
-        console.log('Fetch Error..!!', error);  // Manejo de errores
-        return null;  // En caso de error, devolver null
+        console.log('Fetch Error..!!', error);  
+        return null; 
     }
     
 };
